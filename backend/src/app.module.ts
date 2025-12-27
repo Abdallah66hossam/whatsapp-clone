fa862ab67c7bdc5f6cfb3b42dd8617eaf6bdea8f
Abdallah <abdallahhossam.dev@gmail.com>
2025-12-27 11:35:25 +0200

    backend project setup

---
+++ backend/src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(
+      process.env.MONGODB_URI || 'mongodb://localhost:27017/whatsapp-clone',
+    ),
+  ],
+})
+export class AppModule {}
